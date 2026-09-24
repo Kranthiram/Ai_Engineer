35f1a288ab535698db2c8ac4fbefe7cda649f9e4
Kranthiram <alugulakranthiram@gmail.com>
2026-09-24 15:54:13 +0530

    Add week 1 day 3 token usage learning

---
+++ week1/day3/tokens.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key :
+    raise ValueError("Api key not found")
+client = Groq(api_key=my_api_key)
+model = "openai/gpt-oss-120b"
+
+role = "user"
+prompt1 = "Hi!"
+prompt2 = "explain time travel in 30 words"
+prompt3 = "write essay on india in 1000 words"
+
+prompts = [prompt1,prompt2,prompt3]
+
+for prompt in prompts :
+    message={
+        "role": role,
+        "content":prompt
+    }
+    messages = [message]
+    response = client.chat.completions.create(model=model,messages=messages, max_tokens=500)
+    usage = response.usage
+    print(f"Prompt:{prompt}--> your tokens : {usage.prompt_tokens} completion tokens : {usage.completion_tokens} total tokens  : {usage.total_tokens}\
+    Finish Reason: {response.choices[0].finish_reason}")
+    
